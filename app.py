ea4a3ead62af639ddfbf9c233c79ba989977b46e
alexeybelochenko <alexey.belochenko@gmail.com>
2020-07-02 20:00:00 +0300

    The game features of the hanger work well, need to finalize the mini-game

---
+++ app.py
@@ -0,0 +1,37 @@
+import telebot
+import datetime
+from componets.core import Hangman
+bot = telebot.TeleBot('1287855639:AAE3Oe4FA19WGimahG_YbCVs8Zs4NIG2IHY')
+# new_game_start = None
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, """Привет, это игра Hangman. Введите команду /help для того чтобы получить информацию о других командах. Для того чтобы начать игру /game""")
+
+
+@bot.message_handler(commands=['game'])
+def game(message):
+    global new_game_start
+    new_game_start = Hangman()
+    template = ' '.join(list(new_game_start.template))
+    bot.send_message(message.chat.id, f"Бот загадал тебе слово длиной {new_game_start.word_lenght} символов. Попробуй угадать {template} и оно {new_game_start.word}")
+ 
+
+@bot.message_handler(commands=['get_luck'], func=lambda message: True if message.text.lower() == 'да' else False)
+def minigame(message):
+    bot.send_message(message.chat.id, "Это специальная мини-игра, которая может помочь тебе выиграть, но также можно забрать у тебя одну жить. это мини-игра, кости. Бросаешь - побеждаешь, тут можно выбить один из трёх призов. Хочешь бросить? ")
+
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def handle_text_messages(message):
+    msg = new_game_start.hangman_algorithm(str(message.text.lower()))
+    bot.send_message(message.chat.id, msg)
+
+
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    print(message)
+
+bot.polling(none_stop=True, interval=0)
--- componets/core.py
+++ componets/core.py
@@ -6,7 +6,7 @@ import json
 class Hangman:
     def __init__(self):
         self.word = self.__word_gen()
-        self.word_lenght = len(word)
+        self.word_lenght = len(self.word)
         self.template = "_" * self.word_lenght
         self.is_game = True
         self.life = 5
@@ -23,24 +23,29 @@ class Hangman:
     def find(s, ch):
         return [i for i, ltr in enumerate(s) if ltr == ch]
 
-    @staticmethod
-    def chars_replace(char, indexs):
+
+    def chars_replace(self, char, indexs):
         temp = list(self.template)
         for i in indexs:
             temp[i] = char
         
-        string = "".join(temp)
-        return string
+        self.template = "".join(temp)
 
 
     def hangman_algorithm(self, char):
-        if self.is_game and self.life != 0 and self.template != self.word:
-            indexs = find(default, str(char))
-            if len(indexs) != 0:
-                for i in indexs:
-                    template = chars_replace(char, indexs)
-            else:
-                life -= 1
-                return f'Буквы нет и у вас осталось {life} жизней'
+        if self.template != self.word
+            if self.is_game and self.life > 0:
+                indexs = self.find(self.word, char)
+                if len(indexs) != 0:
+                    for i in indexs:
+                        self.chars_replace(char, indexs)
+
+                    return f'Буква (Буквы) {char} есть в этом слове {self.template}'
+                else:
+                    self.life -= 1
+                    return f'Буквы нет и у вас осталось {self.life} жизней'
+
+            return 'Вы допустили предельное количество ошибок, чтобы начать новую игру /game'
         
-        return 'Вы допустили предельное количество ошибок')
+        else:
+            return f'Вы угадали слово {self.word}'
